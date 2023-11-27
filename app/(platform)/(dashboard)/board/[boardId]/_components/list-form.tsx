8220dcc03741f19f4c4c3d8ce8d3271877c4cb4e
Md Kaish Alam <alamkaishg1511@gmail.com>
2023-11-28 04:55:06 +0530

    completed list actions

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
@@ -1,13 +1,23 @@
 "use client";
 
 import { useState, useRef, ElementRef } from "react";
-import { Plus } from "lucide-react";
+import { Plus, X } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { useEventListener, useOnClickOutside } from "usehooks-ts";
+
+import { Button } from "@/components/ui/button";
+import { FormInput } from "@/components/form/form-input";
+import { FormSubmit } from "@/components/form/form-submit";
+import { useAction } from "@/hooks/use-action";
+import { createList } from "@/actions/create-list";
 
 import { ListWrapper } from "./list-wrapper";
-import { useEventListener, useOnClickOutside } from "usehooks-ts";
-import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
 
 export const ListForm = () => {
+  const router = useRouter();
+  const params = useParams();
+
   const formRef = useRef<ElementRef<"form">>(null);
   const inputRef = useRef<ElementRef<"input">>(null);
 
@@ -24,6 +34,17 @@ export const ListForm = () => {
     setIsEditing(false);
   };
 
+  const { execute, fieldErrors } = useAction(createList, {
+    onSuccess: (data) => {
+      toast.success(`List "${data.title}" in created successfully`);
+      disableEditing();
+      router.refresh();
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
+
   const onKeyDown = (e: KeyboardEvent) => {
     if (e.key === "Escape") {
       disableEditing();
@@ -33,15 +54,49 @@ export const ListForm = () => {
   useEventListener("keydown", onKeyDown);
   useOnClickOutside(formRef, disableEditing);
 
+  const onFormSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string;
+    const boardId = formData.get("boardId") as string;
+
+    execute({ title, boardId });
+  };
+
+  if (isEditing) {
+    return (
+      <ListWrapper>
+        <form
+          action={onFormSubmit}
+          ref={formRef}
+          className="w-full p-3 rounded-md bg-white space-y-4 shadow-md"
+        >
+          <FormInput
+            ref={inputRef}
+            errors={fieldErrors}
+            id="title"
+            className="text-sm px-2 py-1 h-7 font-medium border-transparent hover:border-input focus:border-input transition"
+            placeholder="Enter list title..."
+          />
+          <input hidden value={params.boardId} name="boardId" />
+          <div className="flex items-center gap-x-1">
+            <FormSubmit>Add list</FormSubmit>
+            <Button onClick={disableEditing} size="sm" variant="ghost">
+              <X className="h-5 w-5" />
+            </Button>
+          </div>
+        </form>
+      </ListWrapper>
+    );
+  }
+
   return (
     <ListWrapper>
-      <Button 
+      <button
         className="w-full rounded-md bg-white/80 hover:bg-white/50 transition p-3 flex items-center font-medium text-sm justify-start text-black"
         onClick={enableEditing}
       >
         <Plus className="h-4 w-4 mr-2" />
         Add a list
-      </Button>
+      </button>
     </ListWrapper>
   );
 };
---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
@@ -0,0 +1,117 @@
+"use client";
+
+import { ElementRef, useRef } from "react";
+import { Copy, MoreHorizontal, Plus, Trash2, X } from "lucide-react";
+import { List } from "@prisma/client";
+import { toast } from "sonner";
+
+import {
+  Popover,
+  PopoverClose,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Button } from "@/components/ui/button";
+import { FormSubmit } from "@/components/form/form-submit";
+import { Separator } from "@/components/ui/separator";
+import { useAction } from "@/hooks/use-action";
+import { deleteList } from "@/actions/delete-list";
+import { copyList } from "@/actions/copy-list";
+
+interface ListOptionsProps {
+  onAddCard: () => void;
+  data: List;
+}
+
+export const ListOptions = ({ onAddCard, data }: ListOptionsProps) => {
+  const closeRef = useRef<ElementRef<"button">>(null);
+
+  const { execute: executeDelete } = useAction(deleteList, {
+    onSuccess: (data) => {
+      toast.success(`List "${data.title}" deleted successfully.`);
+      closeRef.current?.click();
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
+
+  const { execute: executeCopy } = useAction(copyList, {
+    onSuccess: (data) => {
+      toast.success(`List "${data.title}" copied successfully.`);
+      closeRef.current?.click();
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
+
+  const handleDelete = (formData: FormData) => {
+    const id = formData.get("id") as string;
+    const boardId = formData.get("boardId") as string;
+
+    executeDelete({ id, boardId });
+  };
+
+  const handleCopy = (formData: FormData) => {
+    const id = formData.get("id") as string;
+    const boardId = formData.get("boardId") as string;
+
+    executeCopy({ id, boardId });
+  };
+
+  
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button className="h-auto w-auto p-2" variant="ghost">
+          <MoreHorizontal className="h-4 w-4" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="px-0 pt-3 pb-3" side="bottom" align="start">
+        <div className="text-sm font-medium text-center text-neutral-600 pb-4">
+          List Actions
+        </div>
+        <PopoverClose ref={closeRef} asChild>
+          <Button
+            className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600"
+            variant="ghost"
+          >
+            <X className="h-4 w-4" />
+          </Button>
+        </PopoverClose>
+        <Button
+          onClick={onAddCard}
+          className="rounded-none w-full h-auto p-2 px-5 justify-start font-normal text-sm"
+          variant="ghost"
+        >
+          <Plus className="h-4 w-4 mr-3 font-normal text-sm text-neutral-600" />
+          Add Card
+        </Button>
+        <form action={handleCopy}>
+          <input hidden name="id" id="id" value={data.id} />
+          <input hidden name="boardId" id="boardId" value={data.boardId} />
+          <FormSubmit
+            variant="ghost"
+            className="rounded-none w-full h-auto p-2 px-5 justify-start font-normal text-sm"
+          >
+            <Copy className="h-4 w-4 mr-3 font-normal text-sm text-neutral-600" />
+            Copy List
+          </FormSubmit>
+        </form>
+        <Separator />
+        <form action={handleDelete}>
+          <input hidden name="id" id="id" value={data.id} />
+          <input hidden name="boardId" id="boardId" value={data.boardId} />
+          <FormSubmit
+            variant="ghost"
+            className="rounded-none w-full h-auto p-2 px-5 justify-start font-normal text-sm"
+          >
+            <Trash2 className="h-4 w-4 mr-3 font-normal text-sm text-neutral-600" />
+            Delete List
+          </FormSubmit>
+        </form>
+      </PopoverContent>
+    </Popover>
+  );
+};
