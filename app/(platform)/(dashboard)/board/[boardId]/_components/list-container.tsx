99b2ad5ee9de3af9bd2fb51a289891291b93545c
Md Kaish Alam <alamkaishg1511@gmail.com>
2023-11-30 01:27:37 +0530

    added the card and list components

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -1,40 +1,86 @@
-'use client'
+"use client";
 
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import { DragDropContext, Droppable } from "@hello-pangea/dnd";
 
-import { ListWithCards } from "@/types"
-import { ListForm } from "./list-form"
-import { ListItem } from "./list-item"
+import { ListWithCards } from "@/types";
+import { ListForm } from "./list-form";
+import { ListItem } from "./list-item";
 
 interface ListContainerProps {
-  data: ListWithCards[],
-  boardId: string
-};
+  data: ListWithCards[];
+  boardId: string;
+}
 
-export const ListContainer = ({
-  data,
-  boardId
-}: ListContainerProps) => {
+function reorder<T>(list: T[], startIndex: number, endIndex: number) {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
 
+  return result;
+}
+
+export const ListContainer = ({ data, boardId }: ListContainerProps) => {
   const [orderedData, setOrderedData] = useState(data);
 
   useEffect(() => {
     setOrderedData(data);
   }, [data]);
 
+  const onDragEnd = (result: any) => {
+    const { destination, source, type } = result;
+
+    if (!destination) return;
+
+    // if dropped in the same position
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    // if user moves a list
+    if(type === 'list') {
+      const items = reorder(
+        orderedData,
+        source.index,
+        destination.index
+      ).map((item, index) => ({ ...item, order: index }));
+
+      setOrderedData(items);
+      // TODO: Trigger server actions
+    }
+
+    // if user moves a card
+    if(type === 'card') {
+      let newOrderedData = [...orderedData];
+
+      // source and the destination list
+      const sourceList = newOrderedData.find(list => list.id === source.droppableId);
+      const destinationList = newOrderedData.find(list => list.id === destination.droppableId);
+    }
+
+  };
+
   return (
-    <ol className="flex gap-x-3 h-full">
-      {orderedData.map((list, index) => {
-        return (
-          <ListItem 
-            key={list.id}
-            index={index}
-            data={list}
-          />
-        )
-      })}
-      <ListForm />
-      <div className="flex-shrink-0 w-1" />
-    </ol>
-  )
-}
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="lists" type="list" direction="horizontal">
+        {(provided) => (
+          <ol
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+            className="flex gap-x-3 h-full"
+          >
+            {orderedData.map((list, index) => {
+              return <ListItem key={list.id} index={index} data={list} />;
+            })}
+            {provided.placeholder}
+            <ListForm />
+            <div className="flex-shrink-0 w-1" />
+          </ol>
+        )}
+      </Droppable>
+    </DragDropContext>
+  );
+};
